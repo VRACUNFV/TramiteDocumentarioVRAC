efdbc6868f12918103b3d86e3fd3c2b0f27dbae4
VRACUNFV <ccamposm@unfv.edu.pe>
2025-02-20 15:54:14 -0500

    Create _document.js

---
+++ pages/_document.js
@@ -0,0 +1,25 @@
+// pages/_document.js
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* Preconexión a Google Fonts */}
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+          {/* Importa la fuente Montserrat */}
+          <link
+            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
