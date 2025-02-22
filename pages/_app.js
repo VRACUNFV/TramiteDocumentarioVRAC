5e609376bbc8c5173ce606c4020f5a6f8b0020e0
VRACUNFV <ccamposm@unfv.edu.pe>
2025-02-21 20:17:03 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,24 @@
 // pages/_app.js
 import * as React from 'react';
 import Head from 'next/head';
+import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import theme from '../theme';
 
-export default function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <>
       <Head>
         <title>Sistema de Alertas VRAC</title>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <SessionProvider session={session}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </SessionProvider>
     </>
   );
 }
