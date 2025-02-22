6291b07281469b2aa3ca185396527c58edf7d36c
VRACUNFV <ccamposm@unfv.edu.pe>
2025-02-21 22:33:15 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,34 @@
 // pages/_app.js
 import * as React from 'react';
 import Head from 'next/head';
-import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import theme from '../theme';
+import theme from '../theme/theme';
 
-export default function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+// NextAuth SessionProvider
+import { SessionProvider } from 'next-auth/react';
+
+// Importamos EmotionCache para MUI
+import createEmotionCache from '../theme/createEmotionCache';
+import { CacheProvider } from '@emotion/react';
+
+const clientSideEmotionCache = createEmotionCache();
+
+export default function MyApp(props) {
+  const { Component, pageProps: { session, ...pageProps }, emotionCache = clientSideEmotionCache } = props;
+
   return (
-    <>
-      <Head>
-        <title>Sistema de Alertas VRAC</title>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-      </Head>
+    <CacheProvider value={emotionCache}>
       <SessionProvider session={session}>
         <ThemeProvider theme={theme}>
+          <Head>
+            <meta name="viewport" content="initial-scale=1, width=device-width" />
+          </Head>
+          {/* CssBaseline elimina estilos por defecto y normaliza */}
           <CssBaseline />
           <Component {...pageProps} />
         </ThemeProvider>
       </SessionProvider>
-    </>
+    </CacheProvider>
   );
 }
