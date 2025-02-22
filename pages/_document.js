7192de3ce77126e4ebdb85f978841706a64635bf
VRACUNFV <ccamposm@unfv.edu.pe>
2025-02-21 22:33:00 -0500

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,17 +1,18 @@
 // pages/_document.js
+import React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import createEmotionServer from '@emotion/server/create-instance';
+import createEmotionCache from '../theme/createEmotionCache';
+import theme from '../theme/theme';
 
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap"
-            rel="stylesheet"
-          />
+          {/* MUI primary color */}
+          <meta name="theme-color" content={theme.palette.primary.main} />
+          {/* Inyecta estilos generados en SSR */}
         </Head>
         <body>
           <Main />
@@ -21,3 +22,37 @@ export default class MyDocument extends Document {
     );
   }
 }
+
+// Extraemos los estilos de MUI generados en el SSR
+MyDocument.getInitialProps = async (ctx) => {
+  const originalRenderPage = ctx.renderPage;
+
+  const cache = createEmotionCache();
+  const { extractCriticalToChunks } = createEmotionServer(cache);
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App) =>
+        function EnhanceApp(props) {
+          return <App emotionCache={cache} {...props} />;
+        },
+    });
+
+  const initialProps = await Document.getInitialProps(ctx);
+  const emotionStyles = extractCriticalToChunks(initialProps.html);
+  const emotionStyleTags = emotionStyles.styles.map((style) => (
+    <style
+      data-emotion={`${style.key} ${style.ids.join(' ')}`}
+      key={style.key}
+      dangerouslySetInnerHTML={{ __html: style.css }}
+    />
+  ));
+
+  return {
+    ...initialProps,
+    styles: [
+      ...React.Children.toArray(initialProps.styles),
+      ...emotionStyleTags,
+    ],
+  };
+};
