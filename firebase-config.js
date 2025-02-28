16c6a3802a04de96f8308c92eba8f517d1961901
VRACUNFV <ccamposm@unfv.edu.pe>
2025-02-27 21:15:45 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,7 @@
 // firebase-config.js
 import { initializeApp } from "firebase/app";
+import { getMessaging, onMessage, getToken } from "firebase/messaging";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBk45KqAWvh52FzOEwKBuEsW8-dAD5V9os",
@@ -12,4 +14,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-export { app };
+const messaging = getMessaging(app);
+const db = getFirestore(app);
+
+export { app, messaging, db, onMessage, getToken };
