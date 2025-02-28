0bcf042fc723aa3252f29704156999f6d91de263
VRACUNFV <ccamposm@unfv.edu.pe>
2025-02-27 21:09:43 -0500

    Update firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,20 +1,21 @@
+// public/firebase-messaging-sw.js
 importScripts("https://www.gstatic.com/firebasejs/9.22.2/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/9.22.2/firebase-messaging-compat.js");
 
-// Inicializa Firebase en el SW con los mismos valores
 firebase.initializeApp({
   apiKey: "AIzaSyBk45KqAWvh52FzOEwKBuEsW8-dAD5V9os",
   authDomain: "vrac-unfv.firebaseapp.com",
   projectId: "vrac-unfv",
   storageBucket: "vrac-unfv.firebasestorage.app",
   messagingSenderId: "612784182198",
-  appId: "1:612784182198:web:e154ed27de26f5760ea717"
+  appId: "1:612784182198:web:e154ed27de26f5760ea717",
+  measurementId: "G-LQ971WJQDQ"
 });
 
 const messaging = firebase.messaging();
 
-// Manejo de mensajes en segundo plano:
 messaging.onBackgroundMessage((payload) => {
   console.log("Mensaje en segundo plano:", payload);
-  // self.registration.showNotification("Título", { body: "Texto" });
+  // Opcional: Mostrar notificación
+  // self.registration.showNotification("Notificación", { body: "Texto de notificación" });
 });
