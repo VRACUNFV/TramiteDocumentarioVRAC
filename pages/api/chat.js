610b6e2fb20a4de793bf89ccdd91959b057209ab
VRACUNFV <ccamposm@unfv.edu.pe>
2025-02-27 20:51:11 -0500

    Update chat.js

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -2,16 +2,18 @@
 import { Server } from "socket.io";
 
 export default function handler(req, res) {
+  // Verifica si ya se creó una instancia de Socket.IO
   if (!res.socket.server.io) {
     console.log("Creando nueva instancia de Socket.IO");
     const io = new Server(res.socket.server);
     res.socket.server.io = io;
 
     io.on("connection", (socket) => {
-      console.log("Cliente conectado", socket.id);
-
+      console.log("Cliente conectado:", socket.id);
+      
+      // Recibir un mensaje del cliente
       socket.on("chat-message", (data) => {
-        // Emite el mensaje a todos los conectados
+        // Reenvía el mensaje a todos los clientes conectados
         io.emit("chat-message", data);
       });
     });
